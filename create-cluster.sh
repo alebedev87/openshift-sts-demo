4b50e4808a616a4439a97d3377df1a502fd24bec
Andrey Lebedev <alebedev@redhat.com>
2023-03-31 14:39:41 +0200

    cluster scripts: more comments

--- create-cluster.sh
+++ create-cluster.sh
@@ -4,15 +4,22 @@
 
 set -e
 
+# 1. Generate install config.
+
 INSTALL_DIR="aleb-sts-a"
 openshift-install create install-config --dir "${INSTALL_DIR}"
 
+# 2. Set CCO to manual mode.
+
 sed -i '4i credentialsMode: Manual' "${INSTALL_DIR}/install-config.yaml"
+
+# 3. Generate cluster manifests and cluster operator STS secret to them.
 
 cd "${INSTALL_DIR}"
 openshift-install create manifests
-
 cp ../manifests/* ./manifests/
 cp -a ../tls .
+
+# 4. Create the cluster.
 
 exec openshift-install create cluster
--- generate-sts-secrets.sh
+++ generate-sts-secrets.sh
@@ -5,20 +5,21 @@
 
 set -e
 
+# 1. Get ccoctl tool.
+
 RELEASE_IMAGE=$(openshift-install version | awk '/release image/ {print $3}')
-
 CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "${RELEASE_IMAGE}" -a .pull-secret)
-
 oc image extract "${CCO_IMAGE}" --file="/usr/bin/ccoctl" -a .pull-secret; chmod 775 ccoctl
 
+# 2. Extract cluster operator's CredentilsRequest.
+
 CRED_REQ_DIR="credrequests"; mkdir "${CRED_REQ_DIR}"
-
 oc adm release extract --credentials-requests --cloud=aws --to="${CRED_REQ_DIR}" --from="${RELEASE_IMAGE}"
-
 cat "${CRED_REQ_DIR}/0000_50_cluster-ingress-operator_00-ingress-credentials-request.yaml"
 
+
+# 3. Create all: AWS Identity provider, AWS S3 bucket, cluster operator roles and generate cluster operator secrets.
+
 ./ccoctl aws create-all --name=albo-sts --region=us-east-2 --credentials-requests-dir="${CRED_REQ_DIR}"
-
 cat manifests/openshift-ingress-operator-cloud-credentials-credentials.yaml
-
 cat manifests/cluster-authentication-02-config.yaml && echo
